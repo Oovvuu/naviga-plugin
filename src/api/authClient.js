a6647e9da0867c9a459354d33120027e2ebb4806
Kyle Benk <kjbenk@gmail.com>
2021-03-30 13:16:38 -0400

    Always prompt the user to login

--- src/api/authClient.js
+++ src/api/authClient.js
@@ -5,6 +5,7 @@ const authClient = new Auth0Client(
     {
         ...api.getConfigValue('en.infomaker.oovvuu').data,
         scope: 'offline_access openid',
+        prompt: 'login',
         useRefreshTokens: true,
     }
 );
